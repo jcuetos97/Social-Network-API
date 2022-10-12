b01650367f1d31f1187d61eb14d7c4e7964ada4c
unknown <jcuetos97@gmail.com>
2022-10-12 16:02:01 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const mongodb = require ("mongodb").MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017/socialnetDB`;
+
+let db; 
+
+mongodb.connect(
+    connectionStringURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      db = client.db();
+      app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${PORT}`);
+      });
+    }
+  );
+   
+app.use(express.json());
